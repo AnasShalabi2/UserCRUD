18ee8bfaaa2632846758eadd24a379b188c87171
Anas Shalabi <anasshalabi429@gmail.com>
2026-05-08 21:08:20 +0300

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace UserCRUD.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
